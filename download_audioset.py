dcfcce9957cb570022ccc6f076061ecc96c21841
yanzhen <yanzhen.lee99@gmail.com>
2021-07-26 16:49:45 +0800

    youtube_dl fix

--- download_audioset.py
+++ download_audioset.py
@@ -13,29 +13,19 @@ def main(args):
     data_type = args.data_type
     workspace = args.workspace
     
-    data_path = os.join(workspace, 'dataset', data_type)
+    # Create directories
+    data_path = os.path.join(workspace, 'dataset', data_type)
     os.makedirs(data_path, exist_ok=True)
     
-    csv_path = os.join(workspace, 'metadata', '{}_set.csv'.format(data_type))
+    csv_path = os.path.join(workspace, 'metadata', '{}_set.csv'.format(data_type))
     df = pd.read_csv(csv_path, header=None)
     distinct_files = df[0].unique()
     distinct_set = [(x, df[1].loc[df[0] == x].unique()[0]) for x in distinct_files]
     print(len(distinct_set))
     
-     # Create directories
     root = os.getcwd()
-    in_dir = '{}/raw_audioset'.format(workspace)
-    os.makedirs(in_dir, exist_ok=True)
-    try:
-        # Change the current working Directory
-        os.chdir('{}/raw_audioset'.format(workspace))
-        print("Directory changed")
-    except OSError:
-        print("Can't change the current working directory")
-
-    duration = str(10)
+    print('CWD:', root)
     
-   
     # Extract videos from YouTube
     audio_downloader = YoutubeDL({'format':'bestaudio'})
     error_count = 0
@@ -43,19 +33,9 @@ def main(args):
     for file in tqdm(distinct_set):
         try:
             URL = 'https://www.youtube.com/watch?v={}'.format(file[0])
-            command = 'youtube-dl ' + '-f "bestaudio/best" ' + '--extract-audio ' + '--audio-format wav ' + '--audio-quality 0 ' + URL +  ' --external-downloader ' + 'ffmpeg ' + '--external-downloader-args ' + '"-ss ' + str(int(file[-1])) + ' -t ' + duration + ' {}/Y{}.wav"'.format(data_path, file[0])
-            print('Command: ', command)
-            p = subprocess.Popen(command, universal_newlines=True, shell=True,
-            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            text = p.stdout.read()
-            retcode = p.wait()
-            print(text)
-            for filename in os.listdir(os.getcwd()):
-                filepath = os.path.join(os.getcwd(), filename)
-                try:
-                    shutil.rmtree(filepath)
-                except OSError:
-                    os.remove(filepath)
+            command = "ffmpeg -ss " + str(int(file[-1])) + " -t 10 -i $(youtube-dl -f 'bestaudio' -g " + URL + ") -ar " + str(16000) + " -- \"" + '{}/{}_{}'.format(data_path, file[0], int(file[-1])) + ".wav\""
+            print('COMMAND:', command)
+            os.system((command))
         except Exception:
             error_count += 1
             print("Couldn\'t download the audio")
